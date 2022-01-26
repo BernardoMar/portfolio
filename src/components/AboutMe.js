8e0b5a575fea4f87e5d4316a8a054d5edc7dbcdd
BernardoMar <bernardomartin92@outlook.com>
2022-01-27 09:58:19 +1100

    buttons and aboutMe changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import "../css/about.css"
+import "../css/about.css";
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 class AboutMe extends Component {
   render () {
@@ -13,9 +14,19 @@ class AboutMe extends Component {
 
               <img className="screenShot" src="../images/screenShot.PNG" alt="screenShot"/>
 
-            <p> I have 5+ years of experience working for top tier corporate law firms as a solicitor. However, soon after I got my hands dirty coding and developing I have happily decided to make a career change into the IT world. I have become obsessed with the capability to create and design behind the different coding languages and the excitement of problem solving and debugging.</p>
-            <p> I have developed significant expertise and skills working with technologies such as HTML, CSS, JavaScript and Ruby, and I keep pushing myself by undertaking new challenges and some of the most updated development courses. On top of that, I carry with me my previous experience as a lawyer which includes team leadership, excellent time management, problem solving and fast-paced team work. </p>
-            <p> At this stage, I am looking forward to meeting and to start working as a full stack developer for a great team of passionate developers who strive for challenging projects and that can provide professional progress. If you would like to hear more from me, please get in touch! </p>
+            <p>
+              I have 5+ years of experience working for top tier corporate law firms as a solicitor. However, soon after I got my hands dirty coding and developing, I have happily decided to make a career change into the IT world. I have become obsessed with the capability to create and design behind the different coding languages and the excitement of problem solving and debugging.
+            </p>
+            <p>
+              I have gained fantastic experience and confidence around both front-end and back-end coding languages following test driven development principles. As a Full Stack Developer, I am passionate about creating good quality and clean code for applications that are practical and efficient for users according to their needs, backgrounds, and skills. On top of that, I carry with me my previous experience as a lawyer which includes team leadership, excellent time management, problem solving and fast-paced teamwork.
+             </p>
+             <p>
+               My main skills include HTML, CSS, front-end frameworks such as React and Vu, NodeJs, MongoDB, GraphQL, Ruby on Rails and coding languages such as JavaScript and Ruby.
+             </p>
+            <p>
+              I am currently looking forward to getting my developer career launched, working as part of a great team of passionate developers who strive for challenging projects and that can provide professional progress. If you want to know more about me, please get in <AnchorLink  href='#contact'>touch!</AnchorLink>.
+            </p>
+
             <p>By the way, you can call me Bernie!</p>
           </div>
           <div class="contactList">
